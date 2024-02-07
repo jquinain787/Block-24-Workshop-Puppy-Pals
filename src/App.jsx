be577efdf9392d7649625c6cdc2561d76a2030c1
jquinain787 <141972848+jquinain787@users.noreply.github.com>
2024-02-07 00:28:02 -0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,37 @@
-// Write your Color component here
-
+import './App.css'
+import { puppyList } from './data.js'
 import { useState } from 'react'
+import './index.css'
 
-const Color = ({color, setSelectedColor}) => {
-  return (
-    <div 
-      className={color} 
-      onClick={() => setSelectedColor(color)} 
-     ></div>
-  )
-}
 
-const App = () => {
-  const [selectedColor, setSelectedColor] = useState("")
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup)
+
   return (
-    <div id="container">
-      <div id="navbar">
-        <div>Currently selected: </div>
-        <div className={selectedColor}>{selectedColor}</div>
-        <div className="red">red</div>
-      </div>
-      <div id="colors-list">{<><Color color="blue" setSelectedColor={selectedColor}/><Color color="black" setSelectedColor={selectedColor}/><Color color="green" setSelectedColor={selectedColor}/></>}</div>
+    <div className="App">
+
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppyList.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
     </div>
   );
-};
+}
 
-export default App;
+export default App
